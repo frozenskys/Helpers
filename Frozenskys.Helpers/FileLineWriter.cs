5c5e85146e5ac3dc0c39494faca18f83e04265f2
Richard Cooper <richard.cooper@bennetts.co.uk>
2017-01-24 08:51:50 +0000

    Initial Commit

---
+++ Frozenskys.Helpers/FileLineWriter.cs
@@ -0,0 +1,20 @@
+﻿namespace Frozenskys.Helpers
+{
+    using System.IO;
+
+    public class FileLineWriter
+    {
+        private string _filename = "";
+        public FileLineWriter(string filename)
+        {
+            _filename = filename;
+        }
+        public void WriteLine(string line)
+        {
+            using (StreamWriter file = new StreamWriter(_filename, true))
+            {
+                file.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ Frozenskys.Helpers/Pinger.cs
@@ -0,0 +1,29 @@
+﻿namespace Frozenskys.Helpers
+{
+    using System.Net;
+    using System.Net.Sockets;
+
+    public class Pinger
+    {
+        public string Send(IPAddress address)
+        {
+            using (var sckt = new TcpClient())
+            {
+                try
+                {
+                    sckt.Connect(address, 443);
+                    if (sckt.Connected)
+                    {
+                        sckt.Close();
+                        return "Ok";
+                    }
+                }
+                catch
+                {
+                    return "No Connection";
+                }
+            }
+            return "Errm";
+        }
+    }
+}
