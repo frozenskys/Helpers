84fa0066e4582483698bb6f5a856d29267f6b29c
Richard Cooper <richard.cooper@bennetts.co.uk>
2017-01-24 10:25:15 +0000

    Add basic unit tests.

---
+++ HelpersTest/PingerTest.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Frozenskys.Helpers;
+using System.Net;
+
+namespace HelpersTest
+{
+    [TestClass]
+    public class PingerTest
+    {
+        Pinger _pinger = new Pinger();
+
+        [TestMethod]
+        public void ConnectToLocalHostFails()
+        {
+            Assert.AreEqual("No Connection", _pinger.Send(IPAddress.Parse("127.0.0.0").MapToIPv4()));    
+        }
+
+        [TestMethod]
+        public void ConnectToGooglePasses()
+        {
+            Assert.AreEqual("Ok", _pinger.Send(IPAddress.Parse("216.58.198.110").MapToIPv4()));
+        }
+    }
+}
